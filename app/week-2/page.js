de39ec075c9d10e5254189fff2148ac418c2dd7b
Nick Graham <nsgraham98@gmail.com>
2025-01-14 10:27:55 -0700

    jan 14th class

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+      <main>
+        <h1>Welcome to week 2</h1>
+        <p><Link href="/">Go to Home</Link></p>
+      </main>
+    );
+  }
