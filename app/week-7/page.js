db1ff8d4ffa52c3ff190b72b132070914690fc32
Nicholas Graham <nsgraham98@gmail.com>
2025-03-12 13:08:54 -0600

    week 7

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,10 +1,23 @@
+"use client";
+import { useState } from 'react';
 import HomeButton from "../components/HomeButton";
+import ItemList from "./item-list";
+import NewItems from "./newItems";
+import itemsData from './items.json';
 
 export default function Page() 
 {
+    const [items, setItems] = useState([...itemsData]);
+
+    const handleAddItem = (item) => {
+        setItems([...items, item]);
+    };
+
     return (
         <main>
+            <NewItems onAddItem={handleAddItem}/>
             <HomeButton/>
+            <ItemList itemsState={items}/>            
         </main>
     )
 }
